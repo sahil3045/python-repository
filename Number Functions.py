5ac03ada6bdc59c1a96c0fc2eca9c6f0df3ce9f1
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-24 20:51:13 +0530

    Number Functions

---
+++ Number Functions.py
@@ -0,0 +1,28 @@
+print(78)
+number = 79
+print(number)
+print(number + 78)
+
+#multiplication
+print(3*3)
+#division
+print(20/4)
+print(20%4)
+
+number2 = str(number)
+print(number2)
+
+#to print a number regardless of its sign
+print(abs(-26))
+#to find greater and lower number
+print(max(5,2,38,19))
+print(min(5,2,38,19))
+#for rounding of number
+print(round(7.8))
+#for getting a binary number of a number
+print(bin(332))
+
+
+from math import *
+
+print(sqrt(9))
---
+++ String fucntions.py
@@ -0,0 +1,32 @@
+print("I am starting a new journey of learning python and mastering it until i make use of it in my project.")
+
+#Variable
+
+a = "My name is Sahil" #while defining a variable do not use brackets.
+b = "My age "
+c = 19 #while assigning a int to a variable do not use brackets and inv commas.
+  #while printing a variable do not use inverted commas.
+print(a+" and i am 19 yrs old.")
+print(b+"is",c)
+
+print("Hi.\nHow are you?")  #\n is used to write on a new line.
+#if you want to use inv comma use backslash before it
+print("Hi.\'How are you")
+
+#some string fucntions
+name = "sahil"
+print(name[2]) #in python counting starts from 0.
+#for upper case
+print(name.upper())
+#for lower case
+print(name.lower())
+#for checking if name is upper or lower case.
+print(name.isupper())
+print(name.islower())
+#for checking length
+print(len(name))
+#for finding the index of a particular letter
+print(name.index("h"))
+#for replacing some element
+print(name.replace("h","k"))
+
