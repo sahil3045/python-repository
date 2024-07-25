804878c8b534da6d92d827e95b28326bcf7b0279
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-25 17:46:43 +0530

    tuples

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+#Tuples are fixed and cannot be edited using functions
+
+nos = (1, 3, 5, 2, 7)
+strings = ("sky", "water", "fire")
+bool = (False, True)
+all = (1, False, "sky")
+print(all)
+
+print(type(bool))
+print(len(nos))
