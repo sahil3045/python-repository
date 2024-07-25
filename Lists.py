2f79a347281ac3ca9fcf9203ee76984031c02f88
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-25 16:07:18 +0530

    Lists functions

---
+++ Lists.py
@@ -0,0 +1,22 @@
+# In lists [] brackets are used.
+
+list1 = ["UK", "US", "India", "Brazil"]
+name = "Sahil"
+age = 19
+print(list1[1][1])
+print(list1[1:4])
+print(type(list1))
+print(type(name))
+print(type(age))
+
+# for changing elements of list
+names = ["Rohan", "Sahil", 3, True]
+names[1] = "Shyam" 
+print(names)
+# from right side the counting start from -1
+print(names[-2])
+print(len(names)) #dosent give ans on index basis.
+
+names1 = list(("Sky", 3, False))
+print(names1)
+
