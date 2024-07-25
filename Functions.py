fc4be28a2078498458efb6acf02fdf180e02b20a
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-25 20:17:13 +0530

    def functions

---
+++ Functions.py
@@ -0,0 +1,16 @@
+def greetings_function(name):
+    print("Welcome " + name + " !!")
+    #it can only take string and not integer because it does not concatenate for using it use str(name)
+greetings_function("Sahil")
+
+
+def add_func(a, b):
+    print(a+b)
+a = int(input("Enter a number: "))
+b = int(input("ENter the number to add: "))
+add_func(a, b)
+
+def greetings_function(*names): #* means user can input many names 
+    print("Welcome " + names[2] + " !!")
+greetings_function("Sahil", "Ram", "Tom")
+    
