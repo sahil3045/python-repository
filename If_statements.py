84880db078d0aea85d1b7da804100d7d4bcc891b
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-26 22:40:30 +0530

    if_else

---
+++ If_statements.py
@@ -0,0 +1,24 @@
+# gives condition
+a = 5
+b = 5
+
+if a>b:
+    print(str(a) + " is greater than " + str(b))
+
+if a == b:
+    print(str(a) + " is equal to " + str(b))
+
+
+if a<b:
+     print(str(a) + " is lesser than " + str(b))
+
+if a>b:
+    print(str(a) + " is greater than " + str(b))
+elif a == b:
+    print(str(a) + " is equal to " + str(b))
+else:
+    print((str(a) + " is smaller than " + str(b)))
+
+
+
+    
