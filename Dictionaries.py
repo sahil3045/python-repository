ae23e712da5367843cdeef0c0944ac55d533e4dc
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-27 10:47:21 +0530

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,8 @@
+dict = {"name": "Ram", "Age": 19, "Qualification": "College", "friends": ["Sam", "Raj", "Rahul"], "Is_tall": True}
+print(dict)
+print(dict["Age"])
+print(len(dict))
+print(type(dict))
+
+x = dict["friends"]
+print(x)
