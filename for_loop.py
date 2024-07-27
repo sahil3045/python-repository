2653404ece548c87489c672aea5e87756865cd3d
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-27 17:11:31 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,23 @@
+for letter in "hello":
+    print(letter)
+
+
+mylist = ["ab", "cd", "ef", "gh"]
+for values in mylist:
+    if values == "ef":
+        break
+    print(values)
+
+
+mydict = {"name" : "sahil", "age" : 19}
+for keys in mydict:
+    print(keys)
+
+for x in range(10):
+    print(x)
+
+for b in range(10, 15):
+    print(b)
+else:
+    print("Finish Looping.")
+
