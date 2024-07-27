04046c0fb2bfea7a83545ef83f2a15e25082d7ba
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-27 17:30:26 +0530

    Create 2d_lists.py

---
+++ 2d_lists.py
@@ -0,0 +1,14 @@
+list1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+print(list1[2])
+print(list1[1][1])
+
+# nested loops in python;
+for list in list1:
+    for row in list:
+        print(row)
+
+
