21c6c9ed8a5f0dbd5af613d0c74ec471e6933c90
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-27 10:25:00 +0530

    Update If_statements.py

--- If_statements.py
+++ If_statements.py
@@ -12,6 +12,8 @@ if a == b:
 if a<b:
      print(str(a) + " is lesser than " + str(b))
 
+
+
 if a>b:
     print(str(a) + " is greater than " + str(b))
 elif a == b:
@@ -19,6 +21,18 @@ elif a == b:
 else:
     print((str(a) + " is smaller than " + str(b)))
 
+value = input("Enter the value: ")
+if type(value) == str:
+    print(value + " is a string.")
 
+else:
+    print(value, " is not a str" )
+
+#to check weather a number is divisible by 5
+number = int(input("Enter the number: "))
+if number%5 == 0:
+    print(number, "Is dvisible by 5.")
+else:
+    print(number, " Is not divisible by 5.")
 
     
