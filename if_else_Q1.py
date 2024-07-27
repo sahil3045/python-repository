21f476c17f05ed7d5437228d925e0d9895ddd5c2
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-27 10:25:12 +0530

    Create if_else_Q1.py

---
+++ if_else_Q1.py
@@ -0,0 +1,7 @@
+#to check if the length of a sentence is less than 10.
+sentence = input(("ENter the sentence: "))
+
+if len(sentence)>10:
+    print("The length of sentence is more than 10.")
+else:
+    print("THe length sentence is less than 10.")
