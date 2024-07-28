b93f593c5d93c7ef952e110c2c973c832b09b966
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-28 11:15:52 +0530

    file_handling

---
+++ Files.py
@@ -0,0 +1,19 @@
+file = open("Newfile.txt", "a")
+
+#print(file.readable())
+#print(file.readlines())
+
+#for lines in file.readlines():
+ #   print(lines)
+#file.close()
+
+
+#r = read
+#w = write
+#a = append
+
+#writing files:
+#file.write("this is the new text")
+file.write("\nthis is the second line")
+
+
