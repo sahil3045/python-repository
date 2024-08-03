491853dafdf3bb4b244b24d76eaa9119b97fd7c1
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-03 13:36:12 +0530

    Create randomisation.py

---
+++ randomisation.py
@@ -0,0 +1,10 @@
+import random 
+
+#integer
+random_int = random.randint(10,20)
+print(random_int)
+
+#float 0.000 to 9.99999
+random_float = random.random()*5   #here *5 is the range and it runs till 4.999...
+print(random_float)
+
